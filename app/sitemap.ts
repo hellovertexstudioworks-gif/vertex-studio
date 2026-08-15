7fbaba9b81defb5b03651f5db1d2750e7ae99b54
Cyril Loon <hello.vertexstudioworks@gmail.com>
2026-08-15 05:32:24 -0400

    Use canonical www URLs in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://vertexstudioworks.com";
+  const baseUrl = "https://www.vertexstudioworks.com";
 
   return [
     {
